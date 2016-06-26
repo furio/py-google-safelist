b4d208c23f413c2b0f2f0322b41862b0cd5721f1
furio <francesco@furio.me>
2016-06-26 17:30:13 +0200

    Get all stuff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import requests
 import ConfigParser
-import responseobjects
 import requestsapi
+import time
+import sys
 
 def getApiKey():
     config = ConfigParser.RawConfigParser()
@@ -10,5 +10,22 @@ def getApiKey():
 
 apiRequest = requestsapi.RequestData(getApiKey(), "Furiosoft")
 threatList = apiRequest.getthreatlists() 
-print threatList
-print apiRequest.getupdateforthreat(threatList[0])
+
+for threat in threatList:
+    clistate = None
+    outfile = open( "save/save-" + threat + ".p", "wb" )
+    sys.stdout.write(threat + ": ")
+    for x in xrange(10):
+        sys.stdout.write(str(x) + " ")
+        data = apiRequest.getupdateforthreat(threat, clistate)
+        if data is not None:
+            outfile.write(str(data) + "\n\n\n\n\n")
+            clistate = data.responses[-1].client['state']
+            time.sleep(data.nextcheck / 1000)
+        else:
+            break
+
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+    outfile.close()
+
