b689a77bb66ed44dd1be8e1a5eba13b803559000
furio <francesco@furio.me>
2016-07-03 16:03:32 +0200

    Moved to levelDb, much better

--- datastore/store.py
+++ datastore/store.py
@@ -1,4 +1,4 @@
-import unqlite
+import plyvel
 import hashlib
 import base64
 
@@ -6,9 +6,9 @@ class ThreatStore(object):
     def __init__(self,dbpath,dbtypes):
         self.__dbpointers = {}
         for x in dbtypes:
-            self.__dbpointers[x] = unqlite.UnQLite(dbpath + '/' + x + ".db")
+            self.__dbpointers[x] = plyvel.DB(dbpath + '/' + x, create_if_missing=True)
       
-            self.__dbpointers['KEEPER'] = unqlite.UnQLite(dbpath + '/' + "master-rescord" + ".db")
+        self.__dbpointers['KEEPER'] = plyvel.DB(dbpath + '/master-records', create_if_missing=True)
 
     def __enter__(self):
         return self
@@ -21,19 +21,23 @@ class ThreatStore(object):
         return self.__dbpointers.keys()
 
     def get(self,store,key):
-        return self.__dbpointers[store].fetch("key" + key)
+        return self.__dbpointers[store].fetch(str(key))
 
     def exist(self,store,key):
-        return self.__dbpointers[store].exists("key" + key)
+        return self.__dbpointers[store].exists(str(key))
 
     def set(self,store,key,val=''):
-        self.__dbpointers[store].store("key" + key,val)
+        self.__dbpointers[store].put(str(key),str(val))
 
     def delete(self,store,key):
-        self.__dbpointers[store].delete("key" + key)
+        self.__dbpointers[store].delete(str(key))
 
     def keyschecksum(self,store,baseencoded=False):
-        hashdata = hashlib.sha256(b''.join([item[0][3:] for item in self.__dbpointers[store]])).digest()
+        keys = []
+        for key,val in self.__dbpointers[store]:
+            keys.extend(key)
+
+        hashdata = hashlib.sha256(b''.join(keys)).digest()
         if baseencoded:
             return base64.b64encode(hashdata)
         else:
